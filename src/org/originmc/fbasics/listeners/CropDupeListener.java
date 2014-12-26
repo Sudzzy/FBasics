2d947cfebe588bfcd42e114e21d9afdd813a07e2
Sudzzy <originmc@outlook.com>
2014-12-26 01:04:33 +0000

    Better Crop Dupe Patch

--- src/org/originmc/fbasics/listeners/CropDupeListener.java
+++ src/org/originmc/fbasics/listeners/CropDupeListener.java
@@ -1,7 +1,6 @@
 package org.originmc.fbasics.listeners;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -18,8 +17,8 @@ import java.util.List;
 
 public class CropDupeListener implements Listener {
 
-
     private final String cropBlock;
+    private List<Material> dupableBlocks = new ArrayList<Material>();
     private List<Material> cropBlocks = new ArrayList<Material>();
 
     public CropDupeListener(FBasics plugin) {
@@ -31,55 +30,42 @@ public class CropDupeListener implements Listener {
 
         for (String block : materials.getStringList("block-placement-near-crops"))
             this.cropBlocks.add(Material.getMaterial(block));
+
+        for (String block : materials.getStringList("crop-blocks"))
+            this.dupableBlocks.add(Material.getMaterial(block));
     }
 
-
+    @SuppressWarnings("deprecation")
     @EventHandler(ignoreCancelled = true)
-    public void onInteract(PlayerInteractEvent e) {
+    public void onInteract(PlayerInteractEvent event) {
+        if (!event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
 
-        Action a = e.getAction();
+        Player player = event.getPlayer();
 
-        if (a.equals(Action.LEFT_CLICK_AIR) || a.equals(Action.LEFT_CLICK_BLOCK) || a.equals(Action.RIGHT_CLICK_AIR)) {
-            return;
+        if (!this.cropBlocks.contains(player.getItemInHand().getType())) return;
+
+        List<Material> materials = new ArrayList<Material>();
+        BlockFace blockFace = event.getBlockFace();
+        Block block = event.getClickedBlock().getRelative(blockFace);
+
+        materials.add(block.getRelative(BlockFace.NORTH).getType());
+        materials.add(block.getRelative(BlockFace.NORTH_EAST).getType());
+        materials.add(block.getRelative(BlockFace.EAST).getType());
+        materials.add(block.getRelative(BlockFace.SOUTH_EAST).getType());
+        materials.add(block.getRelative(BlockFace.SOUTH).getType());
+        materials.add(block.getRelative(BlockFace.SOUTH_WEST).getType());
+        materials.add(block.getRelative(BlockFace.WEST).getType());
+        materials.add(block.getRelative(BlockFace.NORTH_WEST).getType());
+        materials.add(block.getRelative(BlockFace.UP).getType());
+        materials.add(block.getRelative(BlockFace.DOWN).getType());
+
+        for (Material material : materials) {
+            if (this.dupableBlocks.contains(material)) {
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', this.cropBlock));
+                player.updateInventory();
+                event.setCancelled(true);
+                return;
+            }
         }
-
-        Player player = e.getPlayer();
-
-        if (!this.cropBlocks.contains(player.getItemInHand().getType())) {
-            return;
-        }
-
-        BlockFace blockFace = e.getBlockFace();
-        Location location = e.getClickedBlock().getLocation();
-
-        if (blockFace.equals(BlockFace.NORTH)) {
-            location = location.add(0D, 0D, -1D);
-        } else if (blockFace.equals(BlockFace.EAST)) {
-            location = location.add(1D, 0D, 0D);
-        } else if (blockFace.equals(BlockFace.SOUTH)) {
-            location = location.add(0D, 0D, 1D);
-        } else if (blockFace.equals(BlockFace.WEST)) {
-            location = location.add(-1D, 0D, 0D);
-        } else if (blockFace.equals(BlockFace.DOWN)) {
-            location = location.add(0D, -1D, 0D);
-        } else if (blockFace.equals(BlockFace.UP)) {
-            location = location.add(0D, 1D, 0D);
-        }
-
-        if (checkBlock(player.getWorld().getBlockAt(location), Material.CACTUS)
-                || checkBlock(player.getWorld().getBlockAt(location.add(0D, 1D, 0D)), Material.SUGAR_CANE_BLOCK)
-                || player.getWorld().getBlockAt(location).getType().equals(Material.WATER_LILY)) {
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', this.cropBlock));
-            e.setCancelled(true);
-        }
-    }
-
-
-    private boolean checkBlock(Block block, Material material) {
-        return (block.getRelative(BlockFace.DOWN).getType().equals(material)
-                || block.getRelative(BlockFace.NORTH).getType().equals(material)
-                || block.getRelative(BlockFace.EAST).getType().equals(material)
-                || block.getRelative(BlockFace.SOUTH).getType().equals(material)
-                || block.getRelative(BlockFace.WEST).getType().equals(material));
     }
 }
