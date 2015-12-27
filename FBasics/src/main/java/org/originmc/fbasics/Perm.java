1481c95e201c677e74c026219f8aa04b11203de4
Sudzzy <originmc@outlook.com>
2015-12-27 00:07:27 +0000

    Organise permissions

--- FBasics/src/main/java/org/originmc/fbasics/Perm.java
+++ FBasics/src/main/java/org/originmc/fbasics/Perm.java
@@ -7,26 +7,26 @@ public final class Perm {
 
     public final class AntiGlitch {
         public static final String PREFIX = Perm.PREFIX + "antiglitch.";
-        public static final String PHASE = PREFIX + "phase";
-        public static final String FACTION_MAP = PREFIX + "faction-map";
-        public static final String DISMOUNT_CLIPPING = PREFIX + "dismount-clipping";
-        public static final String NETHER_ROOF = PREFIX + "nether-roof";
-        public static final String INVENTORY_DUPE = PREFIX + "inventory-dupe";
-        public static final String CROP_DUPE = PREFIX + "crop-dupe";
         public static final String BOOK_LIMIT = PREFIX + "book-limit";
+        public static final String CROP_DUPE = PREFIX + "crop-dupe";
+        public static final String DISMOUNT_CLIPPING = PREFIX + "dismount-clipping";
         public static final String ENDERPEARLS_COOLDOWN = PREFIX + "enderpearls-cooldown";
-        public static final String ENDERPEARLS_WITHIN_BLOCK = PREFIX + "enderpearls-within-block";
         public static final String ENDERPEARLS_FACTION = PREFIX + "enderpearls-factions";
+        public static final String ENDERPEARLS_WITHIN_BLOCK = PREFIX + "enderpearls-within-block";
+        public static final String FACTION_MAP = PREFIX + "faction-map";
+        public static final String INVENTORY_DUPE = PREFIX + "inventory-dupe";
+        public static final String NETHER_ROOF = PREFIX + "nether-roof";
+        public static final String PHASE = PREFIX + "phase";
     }
 
     public final class Commands {
         public static final String PREFIX = Perm.PREFIX + "commands.";
         public static final String COOLDOWNS = PREFIX + "cooldowns";
-        public static final String WARMUPS = PREFIX + "warmups";
-        public static final String WARMUPS_MOVE = PREFIX + "warmups-move";
-        public static final String WARMUPS_DAMAGE = PREFIX + "warmups-damage";
-        public static final String PRICES = PREFIX + "prices";
         public static final String FACTIONS = PREFIX + "factions";
+        public static final String PRICES = PREFIX + "prices";
+        public static final String WARMUPS = PREFIX + "warmups";
+        public static final String WARMUPS_DAMAGE = PREFIX + "warmups-damage";
+        public static final String WARMUPS_MOVE = PREFIX + "warmups-move";
     }
 
 }
